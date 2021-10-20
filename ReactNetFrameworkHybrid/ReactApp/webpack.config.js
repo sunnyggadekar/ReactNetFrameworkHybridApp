9914f520897a52219e9d5ffbcd1d5b8bb21b0c0e
sunnyggadekar <sunnyggadekar@gmail.com>
2021-10-20 18:33:49 +0530

    created hybrid react .net framework app

---
+++ ReactNetFrameworkHybrid/ReactApp/webpack.config.js
@@ -0,0 +1,22 @@
+﻿module.exports = {
+    context : __dirname,
+    entry : "./app.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ 'babel-preset-env','babel-preset-react']
+                    }
+                }
+            }
+        ]
+    }
+}
